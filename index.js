f82fa147a3fe7e7e285bc8a95603e88da2920eb6
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2021-10-06 22:53:28 +0300

    update origin link

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const router = require("./router/index");
-const { Server } = require('socket.io');
-const { initSocket } = require('./sockets/socket-main');
+const { Server } = require("socket.io");
+const { initSocket } = require("./sockets/socket-main");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: ["http://localhost:3000", "*"], }));
+app.use(cors({ origin: ["https://floating-cliffs-51873.herokuapp.com", "*"] }));
 app.use("/api", router);
 
 const server = app.listen(PORT, () => console.log(`Start on ${PORT}`));
 
 const io = new Server(server, {
   cors: {
-    origin: '*'
-  }
+    origin: "https://floating-cliffs-51873.herokuapp.com",
+  },
 });
 
-
-
-io.on('connection', initSocket);
+io.on("connection", initSocket);
